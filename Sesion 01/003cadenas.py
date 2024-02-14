79b6f96efc1b2b09b0266cd70fd091ca28ad0873
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-14 10:19:25 -0500

    Create 003cadenas.py

---
+++ Sesion 01/003cadenas.py
@@ -0,0 +1,7 @@
+#concatenacion de variables, durante la impresión en pantalla
+
+mensaje1 = '\nBienvenidos'
+mensaje2 = ' a'
+mensaje3 = ' Python\ndesde Skill Perú\n'
+
+print('Impresion: ', mensaje1 + mensaje2 + mensaje3 + 'Fin de la impresion')
