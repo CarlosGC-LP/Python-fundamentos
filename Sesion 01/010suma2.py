09a3076844042f618e26f8ec849353f31261b014
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-14 12:07:56 -0500

    Create 010suma2.py

---
+++ Sesion 01/010suma2.py
@@ -0,0 +1,12 @@
+#Sumaremos dos numeros introducidos desde el teclado
+
+#Entrada de datos
+n1 = int(input('Escribe el primer dato a sumar: '))
+#Se convierte a formato de numero entero
+n2 = int(input('Escribe el segundo dato a suma: '))
+
+#Procesamiento de datos
+resultado = n1 + n2
+
+#Salida de datos
+print('\nEl resultado de la suma es: ', resultado)
