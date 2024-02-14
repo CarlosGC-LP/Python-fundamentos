e45fd578e24fec8d628e53aa5d4e2033f185b248
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-14 10:11:59 -0500

    Create 001cadenas.py

---
+++ Sesion 01/001cadenas.py
@@ -0,0 +1,11 @@
+#Este es nuestro primer programa en Python
+'''
+Este es un comentario
+multilinea
+'''
+
+mensaje1 = 'Bienvenidos'
+mensaje2 = 'a'
+mensaje3 = 'Python'
+
+print(mensaje1, mensaje2, mensaje3)
