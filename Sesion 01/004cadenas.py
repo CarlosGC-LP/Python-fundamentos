c1e02f8f3df9491aae6a00fbe3748e7badb3f26a
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-14 10:29:05 -0500

    Create 004cadenas.py

---
+++ Sesion 01/004cadenas.py
@@ -0,0 +1,6 @@
+#Buscar una subcadena dentro de otra cadena de texto
+
+mensaje = 'Bienvenidos al curso de Python desde Skill'
+subcadena = mensaje.find('Skill')
+
+print('\nLa subcadena buscada esta en la posicion: ', subcadena)
