7d5350621f1321733bac8cbdba22dd8966ef9756
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-14 11:24:02 -0500

    Create 007opArit.py

---
+++ Sesion 01/007opArit.py
@@ -0,0 +1,27 @@
+#Veremos el uso de operadores aritmeticos
+
+print('Exponentes: ')
+
+num = 4
+exponente = 3
+
+resultado = num ** exponente
+
+print('El resultado de la exponenciacion es: ', resultado)
+
+print('\nDivision entera: ')
+
+n1 = 10
+n2 = 3
+
+resultado = n1 // n2
+
+print('La parte entera de la division es: ', resultado)
+
+print('\nModulo o resto de una division: ')
+
+n2 = 2
+
+resultado = n1 % n2
+
+print('El modulo o resto de la division es: ', resultado)
