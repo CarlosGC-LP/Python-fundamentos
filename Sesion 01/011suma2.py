ebc2fed13c8394fc7d6a7c10db9162a7dc398fca
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-14 12:08:27 -0500

    Create 011sum2.py

---
+++ Sesion 01/011suma2.py
@@ -0,0 +1,10 @@
+#Entrada de datos
+n1 = float(input('Escribe el primer dato a sumar: '))
+#Se convierte a formato de numero decimal
+n2 = float(input('Escribe el segundo dato a suma: '))
+
+#Procesamiento de datos
+resultado = n1 + n2
+
+#Salida de datos
+print('\nEl resultado de la suma es: ', resultado)
