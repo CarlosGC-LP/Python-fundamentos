c6d54bfb410303c3d4d940a74d7e33b97ff4cdad
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-15 12:42:15 -0500

    Create 025forFruta.py

---
+++ Sesion 02/025forFruta.py
@@ -0,0 +1,6 @@
+#El programa recorre una estructura de datos de tipo arreglo unidimensional o array, tambien llamado listas, usando el ciclo for
+
+frutas = ['Manzana', 'Durazno', 'Melón', 'Sandia', 'Pera']
+
+for fruta in frutas:
+    print(fruta)
