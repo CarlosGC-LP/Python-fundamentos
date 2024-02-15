8dc2d3a7718a398ada772da25304e49bb8cf157b
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-14 21:58:00 -0500

    Create 013EstCond.py

---
+++ Sesion 02/013EstCond.py
@@ -0,0 +1,7 @@
+#Realiza un programa que avise al usuario si puede votar evaluando su edad
+
+nombre = input('Escribe tu nombre: ')
+edad = int(input('Escribe tu edad: '))
+
+if (edad >= 18):
+    print(nombre, 'ya puede votar, tiene: ', edad, 'años')
