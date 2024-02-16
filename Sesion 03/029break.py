c0e667ab66cbb22f569190662b316e4a9b3493f1
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-16 01:04:14 -0500

    Create 029break.py

---
+++ Sesion 03/029break.py
@@ -0,0 +1,7 @@
+#Ejemplificar el uso de la instrucción break
+
+for i in range(1, 11):
+    if i == 6:
+        print('Saliendo del sistema...')
+        break
+    print('La variable i, tiene el valor de:', i)
