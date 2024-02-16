b8399e0bbfc72909b564a9834924f91da794b38a
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-15 22:16:34 -0500

    Create 026Mayus.py

---
+++ Sesion 03/026Mayus.py
@@ -0,0 +1,7 @@
+#Convertiremos a mayúsculas un texto ingresado por el usuario
+
+datos = input('Ingresa el texto por favor: ')
+
+datos_mayusculas = datos.upper()
+
+print('El texto ingresado, convertido a mayúsculas es:', datos_mayusculas)
