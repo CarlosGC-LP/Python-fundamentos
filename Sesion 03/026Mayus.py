6ee818f4d8d900a5d2d14a69953c1d1e8d83a1d7
XeroLP <86332206+XeroLP@users.noreply.github.com>
2024-02-15 23:21:12 -0500

    Create 026Mayus.py

--- Sesion 03/026Mayus.py
+++ Sesion 03/026Mayus.py
@@ -5,3 +5,8 @@ datos = input('Ingresa el texto por favor: ')
 datos_mayusculas = datos.upper()
 
 print('El texto ingresado, convertido a mayúsculas es:', datos_mayusculas)
+
+#Para convertir en minusculas
+datos_minusculas = datos_mayusculas.lower()
+
+print('El mismo texto convertido en minusculas es:', datos_minusculas)
